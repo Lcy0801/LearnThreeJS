746e1180d757d51935dcfe0c71e95dcbfecba74e
Chaoying <1754129@gmail.com>
2023-05-14 11:13:31 +0800

    更改物体的位置

--- src/components/ThreeContainer.vue
+++ src/components/ThreeContainer.vue
@@ -12,7 +12,6 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(0, 0, 10);
 scene.add(camera);
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-// 
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xbbffaa });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
@@ -24,7 +23,10 @@ controls.update();
 function animate() {
 	requestAnimationFrame(animate);
 	// 先更新轨道控制器对相机的更改 然后重新渲染
-	controls.update();
+    controls.update();
+    // 更改物体的位置
+    cube.position.x += 0.01;
+    cube.position.x = cube.position.x > 5 ? 0 : cube.position.x;
 	renderer.render(scene, camera);
 }
 // 添加坐标轴辅助器
